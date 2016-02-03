9fb62db1b13024fc8441602bc61ba169716ee681
Raymon Chu <jinsong.chu@gmail.com>
2016-02-03 15:15:40 +0800

    test for autobuild

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var os = require("os");
 http.createServer(function(request, response) {
     console.log("New request arrived.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Welcome to jason's website! you are visiting host " + os.hostname() + "\n");
+    response.write("Welcome to my website! You are visiting host " + os.hostname() + ".\n");
     response.end();
 }).listen(80);
 console.log("Server is running, listening on port 80....");
